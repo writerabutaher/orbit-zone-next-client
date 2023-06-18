70403a50715c68860bb93f022180199d1beb7f99
Abu Taher <writerabutaher@gmail.com>
2023-06-18 20:28:08 +0600

    hero

--- components/Home/Hero.jsx
+++ components/Home/Hero.jsx
@@ -7,16 +7,17 @@ const Hero = () => {
       <div className="hidden md:block">
         <img
           className="absolute bg-cover bg-center w-full h-full inset-0"
-          src="./Hero 08.png"
+          src="./assets/Hero 08.png"
         />
       </div>
       <Navbar />
       <div className="relative px-4 xl:px-0 container mx-auto md:flex items-center gap-8">
         <div className="text-color w-full md:w-1/3 pt-16 lg:pt-32 xl:pt-12">
-          <h1 className="text-4xl md:text-4xl lg:text-6xl w-11/12 lg:w-11/12 xl:w-full text-gray-900 font-extrabold f-f-l uppercase">
-            Find Your Perfect Ride, Sell with Ease
+          <h1 className="text-3xl md:text-4xl lg:text-6xl w-11/12 lg:w-11/12 xl:w-full text-gray-900 font-extrabold f-f-l uppercase">
+            <span className="text-purple-700">Find</span> Your Perfect Ride, {}
+            <span className="text-purple-700">Sell</span> with Ease
           </h1>
-          <div className="f-f-r text-base lg:text-lg pb-20 sm:pb-0 pt-10 xl:pt-6">
+          <div className="f-f-r text-base lg:text-lg sm:pb-0 py-10 xl:pt-6">
             <h2>
               Orbit Zone is the go-to online platform for individuals looking to
               buy or sell cars, providing a comprehensive and seamless
@@ -33,9 +34,9 @@ const Hero = () => {
           </div>
         </div>
         <img
-          className="w-full md:mt-0 object-fill md:w-2/3 md:-ml-4 lg:-ml-4 xl:ml-0"
-          src="./super.png"
-          alt="sample page"
+          className="w-full object-fill md:w-2/3 md:-ml-4 lg:-ml-4 xl:ml-0"
+          src="./assets/super.png"
+          alt="super png"
           role="img"
         />
         <button className="md:hidden hover:opacity-90 text-base w-full xl:text-base xl:w-6/12 mt-4 xl:mt-8 f-f-r py-4  bg-purple-700 text-white font-bold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-700 rounded-lg">
