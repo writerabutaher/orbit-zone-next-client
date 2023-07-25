7a3eb7067734393ff800fadee439c1f22edc2006
Abu Taher <writerabutaher@gmail.com>
2023-07-25 21:16:45 +0600

    firebase authentication

---
+++ contexts/AuthProvider.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import React, { useState, createContext, useEffect } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  sendPasswordResetEmail,
+  User,
+} from "firebase/auth";
+import app from "@/config/firebaseConfig";
+
+const auth = getAuth(app);
+
+interface AuthProviderProps {
+  Children: React.ReactNode;
+}
+
+interface AuthContextValue {
+  user: User | null;
+  registerUser: (
+    email: string,
+    password: string,
+    displayName: string
+  ) => Promise<void>;
+  googleSignIn: () => Promise<void>;
+  loginUser: (email: string, password: string) => Promise<void>;
+  logOut: () => Promise<void>;
+  resetPassword: (email: string) => Promise<void>;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const AuthContext = createContext<AuthContextValue>(
+  {} as AuthContextValue
+);
+
+const AuthProvider = ({ Children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const registerUser = async (
+    email: string,
+    password: string,
+    displayName: string
+  ) => {
+    setLoading(true);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      setUser({
+        ...userCredential.user,
+        email,
+        displayName,
+      });
+      await updateProfile(userCredential.user, { displayName: displayName });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const loginUser = async (email: string, password: string) => {
+    setLoading(true);
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const googleSignIn = async () => {
+    const provider = new GoogleAuthProvider();
+    setLoading(true);
+    try {
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const logOut = async () => {
+    setLoading(true);
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const resetPassword = async (email: string) => {
+    setLoading(true);
+    try {
+      await sendPasswordResetEmail(auth, email);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const contextValue: AuthContextValue = {
+    user,
+    registerUser,
+    googleSignIn,
+    loginUser,
+    logOut,
+    resetPassword,
+    loading,
+    setLoading,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>{Children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
