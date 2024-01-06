876990af5ae3ca92ec7064ca2e7db5eed8ff2baa
Abu Taher <writerabutaher@gmail.com>
2024-01-06 10:16:08 +0600

    type error

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,13 +1,4 @@
-"use client";
-
-import useAuth from "@/hooks/useAuth";
-
-interface ChildrenProps {
-  children?: React.ReactNode;
-}
-
 const dashBoardLayout = ({ children }: ChildrenProps) => {
-  useAuth();
   return (
     <div>
       <header>This is header</header>
--- hooks/useAuth.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { AuthContext } from "@/providers/AuthProvider";
-import { useContext } from "react";
-
-export const useAuth = () => {
-  const context = useContext(AuthContext);
-  return context;
-};
